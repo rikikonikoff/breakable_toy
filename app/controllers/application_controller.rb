9d99891bc2cd48e314098cbbaeeb08b23f1fa65b
Riki Konikoff <riki.konikoff@gmail.com>
2017-01-30 19:12:24 -0500

    changes to routes and controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
-  helper_method :current_provider
 
   def current_user
-    @current_user ||=User.find(session[:user_id]) if session[:user_id]
+    if signed_in_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    elsif signed_in_provider
+      @current_user ||= Provider.find(session[:admin_id]) if session[:provider_id]
+    else
+      nil
+    end
   end
 
-  def current_provider
-    @current_provider ||=Provider.find(session[:provider_id]) if session[:provider_id]
+  def signed_in_user
+    !session[:user_id].nil?
+  end
+
+  def signed_in_admin
+    !session[:provider_id].nil?
   end
 end
