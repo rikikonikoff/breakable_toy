9e52e28fb0fcb4a4724499243285bd3625450f9e
Riki Konikoff <riki.konikoff@gmail.com>
2017-01-30 22:00:30 -0500

    added serializers

--- app/serializers/provider_serializer.rb
+++ app/serializers/provider_serializer.rb
@@ -1,5 +1,5 @@
 class ProviderSerializer < ActiveModel::Serializer
-  attributes :id, :name, :bio, :email, :work_address, :city, :state, :zip
+  attributes :id, :name, :bio, :profile_url, :email, :work_address, :city, :state, :zip
 
   has_many :appointments
   has_many :users, through: :appointments
