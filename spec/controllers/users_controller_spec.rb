c44295748f7c549205fb91e04b0b2a384a8f0f8d
Riki Konikoff <riki.konikoff@gmail.com>
2017-02-02 16:10:26 -0500

    fix filenames for codeship

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe UsersController, type: :controller do
 
   describe "#create" do
     it "returns http redirect" do
-      post :create, user: { name: "Foob" }, session: { auth: { "info" => { "name" => "Foob", "email" => "foob@example.com" } } }
+      post :create, user: { name: "Foob" }, session: {
+        auth: { "info" => { "name" => "Foob", "email" => "foob@example.com" } }
+      }
       expect(response).to have_http_status(:redirect)
     end
   end
