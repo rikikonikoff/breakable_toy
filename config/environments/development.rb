49d62e7a5c7a4c1a6faf6dfd45b089bafe1e3b43
Riki Konikoff <riki.konikoff@gmail.com>
2017-02-03 12:02:23 -0500

    ruby syntax update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,8 +19,9 @@ Rails.application.configure do
   # config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'localhost',
-    port:                 1025 }
+    address: 'localhost',
+    port: 1025
+  }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,12 +16,12 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'the-breakfast-club.herokuapp.com',
-    :authentication => :plain
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'the-breakfast-club.herokuapp.com',
+    authentication: :plain
   }
   # config.action_mailer.raise_delivery_errors = false
 
