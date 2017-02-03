87bfffd73f5ff7a98f40f03356bca038e61c29cc
Riki Konikoff <riki.konikoff@gmail.com>
2017-02-02 19:50:46 -0500

    finished mailer setup

--- app/mailers/proivder_mailer.rb
+++ app/mailers/proivder_mailer.rb
@@ -1,16 +1,16 @@
 class ProviderMailer < ApplicationMailer
   default from: 'notifications@example.com'
 
-  def booking_email(provider, appointment)
-    @provider = provider
+  def booking_email(appointment)
     @appointment = appointment
+    @provider = @appointment.provider
     @url = appointment_path(@appointment)
     mail(to: @provider.email, subject: 'Someone Booked An Appointment With You')
   end
 
-  def cancellation_email(provider, appointment)
-    @provider = provider
+  def cancellation_email(appointment)
     @appointment = appointment
+    @provider = @appointment.provider
     @url = appointment_path(@appointment)
     mail(to: @provider.email, subject: 'Your Appointment Was Cancelled By The Patient')
   end
