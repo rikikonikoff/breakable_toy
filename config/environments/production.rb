98d21b5d5e84fdaad7819592889fff22da978ef3
Riki Konikoff <riki.konikoff@gmail.com>
2017-02-07 11:39:07 -0500

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     server: 'smtp.sendgrid.net',
+    host: 'www.gmail.com'
     port: '587',
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
