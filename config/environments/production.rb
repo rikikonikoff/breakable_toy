e8aaa2f74dc8ca3fd0cf36cfffe6bf781b6f39bb
Riki Konikoff <riki.konikoff@gmail.com>
2017-02-07 11:49:00 -0500

    more sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     server: 'smtp.sendgrid.net',
-    host: 'www.gmail.com',
     port: '587',
+    only_path: true,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
     domain: 'accesspoint.herokuapp.com',
