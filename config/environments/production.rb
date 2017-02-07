b2d27e62f6708a735c719d77bb76f55c89af918a
Riki Konikoff <riki.konikoff@gmail.com>
2017-02-07 15:42:56 -0500

    more config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,12 +14,12 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    server: 'smtp.sendgrid.net',
+    address: 'smtp.sendgrid.net',
     port: '587',
+    enable_starttls_auto: true,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
     domain: 'accesspoint.herokuapp.com',
-    address: 'accesspoint.herokuapp.com',
     authentication: :plain
   }
   config.action_mailer.default_url_options = { host: 'accesspoint.herokuapp.com' }
