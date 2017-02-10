70a6d1b9ff7b5e288992002ef587be630e8ca955
Riki Konikoff <riki.konikoff@gmail.com>
2017-02-10 15:29:14 -0500

    hide calendar

--- react/src/main.js
+++ react/src/main.js
@@ -2,16 +2,16 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import Calendar from './components/Calendar'
+// import Calendar from './components/Calendar'
 
-$(function() {
-  if (document.getElementById('calendar')){
-    ReactDOM.render(
-      <Calendar />,
-      document.getElementById('calendar')
-    );
-  }
-});
+// $(function() {
+//   if (document.getElementById('calendar')){
+//     ReactDOM.render(
+//       <Calendar />,
+//       document.getElementById('calendar')
+//     );
+//   }
+// });
 
 $(function() {
   if (document.getElementById('providers')){
