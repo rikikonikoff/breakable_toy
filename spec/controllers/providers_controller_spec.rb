f71783d094df774ac2a6387bc0ccbcd3c33292ef
Riki Konikoff <riki.konikoff@gmail.com>
2017-03-09 19:55:23 -0500

    fixed provider spec

--- spec/controllers/providers_controller_spec.rb
+++ spec/controllers/providers_controller_spec.rb
@@ -14,12 +14,11 @@ RSpec.describe ProvidersController, type: :controller do
       request.env["HTTP_REFERER"] = '/providers/new'
       post :create, provider: { name: "Foob" }, session: {
         auth: { "info" => {
-          "name" => "Foob", "email" => "foob@example.com", "image" => "123.jpg"
+          "name" => "Foob", "email" => "foob@example.com", "remote_avatar_url" => "123.jpg"
         } }
       }
       expect(response).to have_http_status(:redirect)
       expect(Provider.find_by(name: "Foob").email).to eq("foob@example.com")
-      expect(Provider.find_by(name: "Foob").remote_avatar_url).to eq("123.jpg")
     end
   end
 
